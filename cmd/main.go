ee36393fba5500199655f276b556c59de2d9cbb4
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2024-07-28 10:36:37 +0530

    feat(google) added initializers

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
-	google_calendar "calendar_automation/pkg/google"
-	"context"
+	"calendar_automation/pkg/initializers"
 	"fmt"
 	"log"
-	"os"
 	"time"
-
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/calendar/v3"
-	"google.golang.org/api/option"
 )
 
+func init() {
+	initializers.InitializeGoogle()
+}
 func main() {
-	ctx := context.Background()
-	b, err := os.ReadFile("../credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := google_calendar.GetClient(config)
-
-	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Calendar client: %v", err)
-	}
 
 	t := time.Now().Format(time.RFC3339)
-	events, err := srv.Events.List("primary").ShowDeleted(false).
+	events, err := initializers.GoogleService.Events.List("primary").ShowDeleted(false).
 		SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
---
+++ pkg/initializers/google_init.go
@@ -0,0 +1,35 @@
+package initializers
+
+import (
+	google_calendar "calendar_automation/pkg/google"
+	"context"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+var GoogleService *calendar.Service
+
+func InitializeGoogle() {
+	ctx := context.Background()
+	b, err := os.ReadFile("../credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := google_calendar.GetClient(config)
+
+	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+	service = srv
+
+}
